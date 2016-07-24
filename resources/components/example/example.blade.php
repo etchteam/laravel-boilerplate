7eb996e01a8b47ad4e73e1da96502c269dd1a39e
Josh Rose <josh@etchapps.com>
2016-07-24 09:19:15 +0100

    fix: Use versioned images in components

--- resources/components/example/example.blade.php
+++ resources/components/example/example.blade.php
@@ -2,7 +2,7 @@
 
 <section class="{{ $name }}">
   <header class="{{ $name }}__banner">
-    <img class="{{ $name }}__banner-image" src="{{ $data->image_url }}" role="presentation" />
+    <img class="{{ $name }}__banner-image" src="{{ elixir($data->image_url) }}" role="presentation" />
     <h1 class="{{ $name }}__banner-title">{{ $data->title }}</h1>
   </header>
   <div class="{{ $name }}__content">{{ $data->content }}</div>
